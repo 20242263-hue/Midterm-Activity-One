6f41b5832b5fa6e05e86f1550a6d0dd95b8e3730
taglinao_p <taglinao_p@ca01.ub.lan>
2026-03-09 11:44:24 +0800

    Configured the react-router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
+
+export default App;
